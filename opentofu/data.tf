ec000711dcf694b1edeb77c647304f89d1d91bc7
Crong <deuxksy@gmail.com>
2026-06-02 01:33:30 +0900

    feat: Ubuntu 24.04 이미지 조회 데이터 소스 추가

---
+++ opentofu/data.tf
@@ -0,0 +1,21 @@
+# opentofu/data.tf
+
+# AMD용 Ubuntu 24.04 LTS 이미지
+data "oci_core_images" "ubuntu_amd" {
+  compartment_id           = var.compartment_ocid
+  operating_system         = "Canonical Ubuntu"
+  operating_system_version = "24.04"
+  shape                    = "VM.Standard.E2.1.Micro"
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
+}
+
+# ARM용 Ubuntu 24.04 LTS 이미지
+data "oci_core_images" "ubuntu_arm" {
+  compartment_id           = var.compartment_ocid
+  operating_system         = "Canonical Ubuntu"
+  operating_system_version = "24.04"
+  shape                    = "VM.Standard.A1.Flex"
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
+}
